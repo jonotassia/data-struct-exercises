f93bf55295c94110b85516f91cde98d758813bf9
Jono Tassia <jonotassia@gmail.com>
2023-02-02 18:46:50 +1100

    Completed Array section

---
+++ arrays.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::cout;
+using std::vector;
+
+string ReverseString(string s) {
+    string rev_string;
+    for (char letter: s) {
+        rev_string.insert(rev_string.begin(), letter);
+    }
+    return rev_string;
+}
+
+void PrintString(string reverse_string) {
+    try {
+        cout << reverse_string << "\n";
+    }
+    catch (...) {
+        cout << "Not a string." << "\n";
+    }
+}
+
+vector<int> MergeSorted(vector<int> v1, vector<int> v2) {
+    int total_size = v1.size() + v2.size();
+    vector<int> merged_vector;
+    for (int i: v1) {
+        for (int j: v2) {
+            if (j < i) {
+                merged_vector.push_back(j);
+            }
+        }
+        merged_vector.push_back(i);
+    }
+    return merged_vector;
+}
+
+void PrintVector(vector<int> v) {
+    for (int i: v) {
+        cout << i;
+    }
+    cout << "\n";
+}
+
+int main() {
+    PrintString(ReverseString("I am called Jono"));
+    PrintVector(MergeSorted({0, 3, 4, 31}, {4, 6, 30}));
+}
+
