2a50a6a2682cf8aa8250d9d098df4c3f2b79ad24
Jono Tassia <jonotassia@gmail.com>
2023-02-07 20:33:50 +1100

    Added print method

--- linked_lists.cpp
+++ linked_lists.cpp
@@ -21,39 +21,49 @@ class LinkedList {
         LinkedList(Node h) : head(h), tail(head) {};
 
         void Append(int value) {
-            Node new_node(value, NULL);
+            Node* new_node = new Node(value, NULL);
             Node node = this->tail;
 
-            node.next_node = &new_node;
-            this->tail = new_node;
+            node.next_node = new_node;
+            this->tail = *new_node;
             this->length++;
         }
 
         void Prepend(int value) {
-            Node new_node(value, NULL);
+            Node* new_node = new Node(value, NULL);
             Node node = this->head;
 
-            new_node.next_node = &node;
-            this->head = new_node;
+            new_node->next_node = &node;
+            this->head = *new_node;
             this->length++;
         }
 
         void Insert(int value, int position) {
-            Node new_node(value, NULL);
-            Node node = this->head;
+            Node* new_node = new Node(value, NULL);
+            Node* node;
+            *node = this->head;
 
-            if (position >= this->length) {
+            if (position >= this->length || position < 0) {
                 cout << "Out of bounds";
                 return;
             }
             
             for (int i = 0; i < position; i++) {
-                node = *node.next_node;
+                node = node->next_node;
             }
             
-            new_node.next_node = node.next_node;
-            node.next_node = &new_node;
+            new_node->next_node = node->next_node;
+            node->next_node = new_node;
             this->length++;
+        }
+
+        void PrintList() {
+            Node* curr_node;
+            *curr_node = this->head;
+            while (curr_node->next_node) {
+                curr_node = curr_node->next_node;
+                cout << curr_node->value << " ";
+            }
         }
 };
 
@@ -62,7 +72,6 @@ int main() {
     list.Prepend(4);
     list.Append(2);
     list.Insert(5, 1);
-    cout << list.length << "\n";
-    cout << list.head.next_node << "\n";
+    list.PrintList();
     list.Insert(0, 4);
 }
