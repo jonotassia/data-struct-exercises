e0c31fb64ac72ee0f85813fd20a612b02fd98abf
Jono Tassia <jonotassia@gmail.com>
2023-02-07 18:17:23 +1100

    Updated linked list methods

--- linked_lists.cpp
+++ linked_lists.cpp
@@ -18,34 +18,51 @@ class LinkedList {
         Node tail;
         int length{1};
 
-        LinkedList(Node h) : head(h), tail(h) {};
+        LinkedList(Node h) : head(h), tail(head) {};
 
         void Append(int value) {
             Node new_node(value, NULL);
-            Node* node = this->tail.next_node;
+            Node node = this->tail;
 
-            node->next_node = &new_node;
+            node.next_node = &new_node;
             this->tail = new_node;
             this->length++;
         }
 
-        void Insert(int value) {
+        void Prepend(int value) {
             Node new_node(value, NULL);
-            Node* node = this->head.next_node;
-            
-            while (node->next_node) {
-                node = node->next_node;
-            }
+            Node node = this->head;
 
-            node->next_node = &new_node;
-            this->tail = new_node;
+            new_node.next_node = &node;
+            this->head = new_node;
+            this->length++;
+        }
+
+        void Insert(int value, int position) {
+            Node new_node(value, NULL);
+            Node node = this->head;
+
+            if (position >= this->length) {
+                cout << "Out of bounds";
+                return;
+            }
+            
+            for (int i = 0; i < position; i++) {
+                node = *node.next_node;
+            }
+            
+            new_node.next_node = node.next_node;
+            node.next_node = &new_node;
             this->length++;
         }
 };
 
 int main() {
     LinkedList list(Node(6, NULL));
+    list.Prepend(4);
     list.Append(2);
+    list.Insert(5, 1);
     cout << list.length << "\n";
     cout << list.head.next_node << "\n";
+    list.Insert(0, 4);
 }
