20fd12fdaa65aa98830524f1c40986069f7c2ee8
Jono Tassia <jonotassia@gmail.com>
2023-02-08 14:23:18 +1100

    Updated linked list logic

--- linked_lists.cpp
+++ linked_lists.cpp
@@ -14,37 +14,49 @@ class Node {
 
 class LinkedList {
     public:
-        Node head;
-        Node tail;
+        Node* head;
+        Node* tail;
         int length{1};
 
-        LinkedList(Node h) : head(h), tail(head) {};
+        LinkedList(int value) {
+            Node* new_node = new Node(value, NULL);
+            head = new_node;
+            tail = new_node;
+        };
 
         void Append(int value) {
             Node* new_node = new Node(value, NULL);
-            Node node = this->tail;
 
-            node.next_node = new_node;
-            this->tail = *new_node;
+            this->tail->next_node = new_node;
+            this->tail = new_node;
             this->length++;
         }
 
         void Prepend(int value) {
             Node* new_node = new Node(value, NULL);
-            Node node = this->head;
 
-            new_node->next_node = &node;
-            this->head = *new_node;
+            new_node->next_node = this->head;
+            this->head = new_node;
             this->length++;
         }
 
         void Insert(int value, int position) {
             Node* new_node = new Node(value, NULL);
             Node* node;
-            *node = this->head;
+            node = this->head;
 
             if (position >= this->length || position < 0) {
                 cout << "Out of bounds";
+                return;
+            }
+
+            if (position == 0) {
+                this->Prepend(value);
+                return;
+            }
+
+            if (position == this->length) {
+                this->Append(value);
                 return;
             }
             
@@ -57,21 +69,39 @@ class LinkedList {
             this->length++;
         }
 
+        void Remove(int position) {
+            Node* node;
+            node = this->head;
+
+            for (int i = 0; i < position; i++) {
+                node = node->next_node;
+            }
+
+            Node* rem_node = node->next_node;
+            node->next_node = rem_node->next_node;
+            delete rem_node;
+        }
+
         void PrintList() {
             Node* curr_node;
-            *curr_node = this->head;
+            curr_node = this->head;
             while (curr_node->next_node) {
-                curr_node = curr_node->next_node;
                 cout << curr_node->value << " ";
+                curr_node = curr_node->next_node;
             }
+            cout << curr_node->value << "\n";
         }
 };
 
 int main() {
-    LinkedList list(Node(6, NULL));
-    list.Prepend(4);
+    LinkedList list(6);
     list.Append(2);
+    list.PrintList();
+    list.Prepend(4);
+    list.PrintList();
     list.Insert(5, 1);
+    list.PrintList();
+    list.Remove(1);
     list.PrintList();
     list.Insert(0, 4);
 }
